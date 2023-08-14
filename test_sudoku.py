f08d273f64eeb981ca4cf400deba52023231ead3
Tatsuya Yatagawa <tatsy.mail@gmail.com>
2023-08-14 18:55:15 +0900

    Update test code.

--- test_sudoku.py
+++ test_sudoku.py
@@ -1,5 +1,6 @@
 import os
 import re
+import glob
 
 import cv2
 import numpy as np
@@ -8,44 +9,54 @@ from sudoku import solve
 
 CUR_DIR = os.path.dirname(os.path.abspath(__file__))
 
-file_paths = [
-    "./data/level_1.jpg",
-    "./data/level_2.jpg",
-    "./data/level_3.jpg",
-]
 
-answers = [""] * 3
+def get_test_data():
+    levels = [1, 2, 3]
+    data = []
+    for level in levels:
+        image_paths = glob.glob(os.path.join(CUR_DIR, "data/level{:d}/*.jpg".format(level)))
+        data.extend([(path, level) for path in image_paths])
 
-ans = """
-2 3 5 7 9 1 6 4 8
-1 6 9 4 2 8 7 5 3
-7 4 8 6 3 5 9 2 1
-3 9 6 2 7 4 8 1 5
-5 2 1 3 8 9 4 6 7
-4 8 7 1 5 6 3 9 2
-8 5 3 9 6 2 1 7 4
-9 7 4 5 1 3 2 8 6
-6 1 2 8 4 7 5 3 9
-"""
-
-answers = [ans] * len(file_paths)
-
-num_dict = {str(i): i for i in range(1, 10)}
+    return data
 
 
-@pytest.mark.parametrize("path, answer", zip(file_paths, answers))
-def test_solve(path: str, answer: str):
-    image = cv2.imread(os.path.join(CUR_DIR, path), cv2.IMREAD_COLOR)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    actual = solve(image)
+class Sudoku(object):
+    def __init__(self, image_path: str, answer_path: str) -> None:
+        self.image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+        self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
 
-    assert isinstance(actual, np.ndarray), "Return NumPy's NDArray!"
-    assert actual.dtype == np.int32, "Return NumPy array with int32 data type!"
-    assert actual.ndim == 2, "#dimensions of NumPy array must be 2!"
-    assert actual.shape[0] == 9 and actual.shape[1] == 9, "Size of the NumPy array must be 9x9!"
+        num_dict = {str(i): i for i in range(1, 10)}
+        with open(answer_path, "r") as f:
+            ans_text = f.read()
 
-    expected = re.split(r"[\s]{1,2}", answer)
-    expected = [num_dict[c] for c in expected if c != ""]
-    expected = np.array(expected, dtype="int32").reshape((9, 9))
+        ans_chars = re.split(r"[\s]+", ans_text)
+        ans_chars = [c for line in ans_chars for c in line]
+        ans_chars = [num_dict[c] for c in ans_chars if c != ""]
+        self.answer = np.array(ans_chars, dtype="int32").reshape((9, 9))
 
-    assert np.all(expected == actual), "Your answer is wrong!!"
+
+@pytest.fixture(scope="session")
+def score():
+    score = np.zeros((1), dtype="int32")
+    yield score
+    print("\n\nYour score is {:d}".format(score.item()))
+
+
+@pytest.mark.parametrize("data", get_test_data())
+def test_solve(data, score):
+    image_path, level = data
+    basename = os.path.splitext(image_path)[0]
+    answer_path = basename + "_answer.txt"
+    sudoku = Sudoku(image_path, answer_path)
+
+    output = solve(sudoku.image)
+
+    assert isinstance(output, np.ndarray), "Return NumPy's NDArray!"
+    assert output.dtype == np.int32, "Return NumPy array with int32 data type!"
+    assert output.ndim == 2, "#dimensions of NumPy array must be 2!"
+    assert output.shape[0] == 9 and output.shape[1] == 9, "Size of the NumPy array must be 9x9!"
+
+    if np.all(output == sudoku.answer):
+        score += level
+    else:
+        pytest.fail("Your output does not match the answer!!")
